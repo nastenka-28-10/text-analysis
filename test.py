609893b56c5bfcae7dcd8eff41f22c3b35158321
nastenka-28-10 <nastenka28.10.96@gmail.com>
2017-12-11 05:42:31 +0300

    without uploader

--- test.py
+++ test.py
@@ -28,14 +28,5 @@ class FlaskTest(unittest.TestCase):
         result = self.app.get('/about')
         self.assertEqual(result.status_code, 200)
 
-    def test_post_upload_request(self):
-        """Assert that user is redirected with status 302 after creating a todo item"""
-        data = dict(
-            file=(BytesIO(b'uploading .txt file'), "file.txt")
-        )
-        response = self.app.post('/uploader', content_type='multipart/form-data',data=data)
-        self.assertEqual(response.status_code, 200)
-
-
 if __name__ == '__main__':
     unittest.main()
